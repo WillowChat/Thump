4e1ae900ad24cbc29d6616a2e436a1f16d401f0c
Sky <git@bunnies.io>
2015-05-29 05:45:41 +0100

    Sync with Warren's ACTION support. Closes #9

--- src/main/java/engineer/carrot/warren/thump/listener/MessageListener.java
+++ src/main/java/engineer/carrot/warren/thump/listener/MessageListener.java
@@ -4,7 +4,9 @@ import com.google.common.eventbus.Subscribe;
 import engineer.carrot.warren.thump.connection.ConnectionManager;
 import engineer.carrot.warren.thump.util.helper.LogHelper;
 import engineer.carrot.warren.thump.util.helper.PlayerHelper;
+import engineer.carrot.warren.warren.event.ChannelActionEvent;
 import engineer.carrot.warren.warren.event.ChannelMessageEvent;
+import engineer.carrot.warren.warren.event.PrivateActionEvent;
 import engineer.carrot.warren.warren.event.PrivateMessageEvent;
 
 public class MessageListener {
@@ -29,7 +31,26 @@ public class MessageListener {
     }
 
     @Subscribe
+    public void handleChannelAction(ChannelActionEvent event) {
+        String user = event.fromUser.getNameWithoutAccess();
+
+        if (manager.usernameMatchesAnyConnection(user)) {
+            return;
+        }
+
+        String output = event.channel.toString() + ": * " + user + " " + event.contents;
+        LogHelper.info(output);
+
+        PlayerHelper.sendMessageToAllPlayers(output);
+    }
+
+    @Subscribe
     public void handlePrivateMessage(PrivateMessageEvent event) {
         LogHelper.info("PM from {}: {}", event.fromUser, event.contents);
+    }
+
+    @Subscribe
+    public void handlePrivateAction(PrivateActionEvent event) {
+        LogHelper.info("PM ACTION from {}: {}", event.fromUser, event.contents);
     }
 }
