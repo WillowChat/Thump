da1f4478ae7aa4a868ba8b0b35c27982ca43d57f
Sky Welch <git@bunnies.io>
2017-01-15 19:27:07 +0000

    Add prefix configuration option for commands

--- src/main/kotlin/engineer/carrot/warren/thump/config/CommandsConfiguration.kt
+++ src/main/kotlin/engineer/carrot/warren/thump/config/CommandsConfiguration.kt
@@ -5,14 +5,17 @@ import net.minecraftforge.common.config.Configuration
 
 class CommandsConfiguration(configuration: Configuration) {
     var players = true
+    var prefix = "!"
 
     init {
-        configuration.setCategoryPropertyOrder(CATEGORY, Lists.newArrayList(PLAYERS_KEY))
+        configuration.setCategoryPropertyOrder(CATEGORY, Lists.newArrayList(PREFIX_KEY, PLAYERS_KEY))
+        this.prefix = configuration.getString(PREFIX_KEY, CATEGORY, this.prefix, "")
         this.players = configuration.getBoolean(PLAYERS_KEY, CATEGORY, this.players, "")
     }
 
     companion object {
         val CATEGORY = "commands"
+        private val PREFIX_KEY = "Prefix"
         private val PLAYERS_KEY = "Players"
     }
 }
